80cc5a871997b43a0be219c088331beafec5cbcf
Dov Vachman <dovach@gmail.com>
2018-04-22 17:52:03 +0000

    second commit

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -1,22 +1,27 @@
 #include <stdio.h>
+#include "CircularInt.hpp"
+using namespace std;
+         
+        CircularInt CircularInt::operator + (CircularInt const &obj) {return obj;}
+        CircularInt CircularInt::operator - (CircularInt const &obj) {return obj;}
+        CircularInt CircularInt::operator / (CircularInt const &obj) {return obj;}
+        CircularInt CircularInt::operator * (CircularInt const &obj) {return obj;}
+        CircularInt CircularInt::operator % (CircularInt const &obj) {return obj;}
+        CircularInt CircularInt::operator ^ (CircularInt const &obj) {return obj;}
+        CircularInt CircularInt::operator + (int a) {return *this;}
+        CircularInt CircularInt::operator - (int a) {return *this;}
+        CircularInt CircularInt::operator / (int a) {return *this;}
+        CircularInt CircularInt::operator * (int a) {return *this;}
+        CircularInt CircularInt::operator % (int a) {return *this;}
+        CircularInt CircularInt::operator ^ (int a) {return *this;}
+        CircularInt CircularInt::operator++(){return *this;}
+        CircularInt CircularInt::operator -- () {return *this;}
+        CircularInt CircularInt::operator-()  {return *this;}
+        CircularInt CircularInt::operator ++ (int) {return *this;}
+        CircularInt CircularInt::operator -- (int) {return *this;}
+        CircularInt CircularInt::operator += (int a) {return *this;}
+        CircularInt CircularInt::operator -= (int a) {return *this;}
+        CircularInt CircularInt::operator *= (int a) {return *this;}
 
-        CircularInt::CircularInt (int a ,int b);    
-        CircularInt CircularInt::operator + (CircularInt const &obj) {return obj}
-        CircularInt CircularInt::operator - (CircularInt onst &obj) {return obj}
-        CircularInt CircularInt::operator / (CircularInt onst &obj) {return obj}
-        CircularInt CircularInt::operator * (CircularInt onst &obj) {return obj}
-        CircularInt CircularInt::operator % (CircularInt onst &obj) {return obj}
-        CircularInt CircularInt::operator ^ (CircularInt onst &obj) {return obj}
-        CircularInt CircularInt::operator++(){return obj}
-        CircularInt CircularInt::operator -- () {return obj}
-        CircularInt CircularInt::operator ++ (int) {return obj}
-        CircularInt CircularInt::operator -- (int) {return obj}
 
-        ostream& CircularInt::operator<< (ostream& os, CircularInt s) {
-           std::operator<<(os,s);
-        return os;
-        }
-}
-main() {
-    
-}
+
--- CircularInt.hpp
+++ CircularInt.hpp
@@ -3,28 +3,74 @@
 #include <iostream>
 using namespace std;
 
-public class CircularInt {
+class CircularInt {
+        private:
+    int _min;
+    int _max;
+    int _CurVal;
+
+
     public:
-        CircularInt (int a ,int b);    
+        //Ctr:
+        CircularInt (int a ,int b){  
+            _min=a;
+            _max=b;
+            this->_CurVal = _min;
+        }
+        
+        CircularInt (int a ,int b,int c){  
+            _min=a;
+            _max=b;
+            this->_CurVal = ((c-1)%b)+1;
+        }
+        
+        //getters:
+	    int CurVal() const {
+		return _CurVal;
+	    }
+	    int min() const {
+		return _min;
+	    }
+	    int max() const {
+		return _max;
+	    }
+
+
         CircularInt operator + (CircularInt const &obj) ;
-        CircularInt operator - (CircularInt onst &obj) ;
-        CircularInt operator / (CircularInt onst &obj) ;
-        CircularInt operator * (CircularInt onst &obj) ;
-        CircularInt operator % (CircularInt onst &obj) ;
-        CircularInt operator ^ (CircularInt onst &obj) ;
-        CircularInt operator++();
-        CircularInt operator -- () ;
+        CircularInt operator - (CircularInt const &obj) ;
+        CircularInt operator / (CircularInt const &obj) ;
+        CircularInt operator * (CircularInt const &obj) ;
+        CircularInt operator % (CircularInt const &obj) ;
+        CircularInt operator ^ (CircularInt const &obj) ;
+        CircularInt operator + (int a) ;
+        CircularInt operator - (int a) ;
+        CircularInt operator / (int a) ;
+        CircularInt operator * (int a) ;
+        CircularInt operator % (int a) ;
+        CircularInt operator ^ (int a) ;
+        CircularInt operator ++();
+        CircularInt operator --() ;
+        CircularInt operator -() ;
         CircularInt operator ++ (int) ;
         CircularInt operator -- (int) ;
-        ostream& operator<< (ostream& os, CircularInt s) ;
-
-    private:
-    int min;
-    int max;
-    int CurVal;
+        CircularInt operator += (int a) ;
+        CircularInt operator -= (int a) ;
+        CircularInt operator *= (int a) ;
+        ostream& operator<< (ostream& os) ;
 
 
 };
+inline ostream& operator<< (ostream& os, const CircularInt& c) {
+
+    os << c.CurVal() ;
+    return os;
+}
+inline const CircularInt operator+(int a, const CircularInt& c2) {
+    return CircularInt(c2.min(),c2.max(),c2.CurVal()+a);
+}
+inline const CircularInt operator-(int a, const CircularInt& c2) {
+    return CircularInt(c2.min(),c2.max(),c2.CurVal()-a);
+}
 //<	>	<=	>=	++	--
 //<<	>>	==	!=
 //+=	-=	/=	%=	^=	&=
