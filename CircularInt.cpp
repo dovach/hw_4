704585aff8df70ba59866cdca30d55d5951625f8
Dov Vachman <dovach@gmail.com>
2018-04-29 17:57:22 +0000

    Third Commit

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -1,27 +1,197 @@
 #include <stdio.h>
+#include<iostream>
 #include "CircularInt.hpp"
 using namespace std;
+// C++ program to do modular division
+
+// C function for extended Euclidean Algorithm
+int gcdExtended(int a, int b, int *x, int *y);
+ 
+// Function to find modulo inverse of b. It returns
+// -1 when inverse doesn't
+int modInverse(int b, int m)
+{
+    int x, y; // used in extended GCD algorithm
+    int g = gcdExtended(b, m, &x, &y);
+ 
+    // Return -1 if b and m are not co-prime
+    if (g != 1)
+        return -1;
+ 
+    // m is added to handle negative x
+    return (x%m + m) % m;
+}
+ 
+// Function to compute a/b under modlo m
+int modDivide(int a, int b, int m)
+{
+    a = a % m;
+    int inv = modInverse(b, m);
+    if (inv == -1)
+        throw new std::string("Division not defined");
+    else
+       return inv;
+}
+ 
+// C function for extended Euclidean Algorithm (used to
+// find modular inverse.
+int gcdExtended(int a, int b, int *x, int *y)
+{
+    // Base Case
+    if (a == 0)
+    {
+        *x = 0, *y = 1;
+        return b;
+    }
+ 
+    int x1, y1; // To store results of recursive call
+    int gcd = gcdExtended(b%a, a, &x1, &y1);
+ 
+    // Update x and y using results of recursive
+    // call
+    *x = y1 - (b/a) * x1;
+    *y = x1;
+ 
+    return gcd;
+}
          
-        CircularInt CircularInt::operator + (CircularInt const &obj) {return obj;}
-        CircularInt CircularInt::operator - (CircularInt const &obj) {return obj;}
-        CircularInt CircularInt::operator / (CircularInt const &obj) {return obj;}
-        CircularInt CircularInt::operator * (CircularInt const &obj) {return obj;}
-        CircularInt CircularInt::operator % (CircularInt const &obj) {return obj;}
-        CircularInt CircularInt::operator ^ (CircularInt const &obj) {return obj;}
-        CircularInt CircularInt::operator + (int a) {return *this;}
-        CircularInt CircularInt::operator - (int a) {return *this;}
-        CircularInt CircularInt::operator / (int a) {return *this;}
-        CircularInt CircularInt::operator * (int a) {return *this;}
-        CircularInt CircularInt::operator % (int a) {return *this;}
-        CircularInt CircularInt::operator ^ (int a) {return *this;}
-        CircularInt CircularInt::operator++(){return *this;}
-        CircularInt CircularInt::operator -- () {return *this;}
-        CircularInt CircularInt::operator-()  {return *this;}
-        CircularInt CircularInt::operator ++ (int) {return *this;}
-        CircularInt CircularInt::operator -- (int) {return *this;}
-        CircularInt CircularInt::operator += (int a) {return *this;}
-        CircularInt CircularInt::operator -= (int a) {return *this;}
-        CircularInt CircularInt::operator *= (int a) {return *this;}
+        CircularInt CircularInt::operator + (CircularInt const &obj) {
+                int a=this->min();
+                int b=this->max();
+                int c=this->CurVal()+obj.CurVal();
+                //cout << CurVal() << "+" <<obj.CurVal() <<"=" << endl; 
+                CircularInt ans = CircularInt(a,b,c);
+                return ans;
+        }
+        CircularInt CircularInt::operator - (CircularInt const &obj) {
+                int a=this->min();
+                int b=this->max();
+                int c=this->CurVal()-obj.CurVal();
+                return  CircularInt(a,b,c);;
+        }
+        CircularInt CircularInt::operator / (CircularInt const &obj) {
+                if(modDivide(_CurVal,obj._CurVal,_max)!=-1){
+                        int ans=modDivide(_CurVal,obj._CurVal,_max);
+                        return CircularInt(_min,_max,ans);
+                }else{
+                        throw new std::string("Division not defined");
+                        return CircularInt(_min,_max,_CurVal);
+                }
+
+                
+        }
+        CircularInt CircularInt::operator * (CircularInt const &obj) {
+               int a=this->min();
+                int b=this->max();
+                int c=this->CurVal()*obj.CurVal();
+                return  CircularInt(a,b,c);
+                
+        }
+        CircularInt CircularInt::operator % (CircularInt const &obj) {
+                int a=this->min();
+                int b=this->max();
+                int c=this->CurVal()%obj.CurVal();
+                return  CircularInt(a,b,c);
+        }
+        CircularInt CircularInt::operator ^ (CircularInt const &obj) {
+                int a=this->min();
+                int b=this->max();
+                int c=this->CurVal()^obj.CurVal();
+                return  CircularInt(a,b,c);
+                
+        }
+        CircularInt CircularInt::operator + (int a) {
+                int x=this->min();
+                int b=this->max();
+                int c=this->CurVal()+a;
+                return  CircularInt(a,b,c);
+                
+        }
+        CircularInt CircularInt::operator - (int a) {
+                int x=this->min();
+                int b=this->max();
+                int c=this->CurVal()-a;
+                return  CircularInt(x,b,c);
+                
+        }
+        CircularInt CircularInt::operator / (int a) {
+                if(modDivide(_CurVal,a,_max)!=-1){
+                        int ans=modDivide(_CurVal,a,_max);
+                        return CircularInt(_min,_max,ans);
+                }else{
+                        throw new std::string("Division not defined");
+                        return CircularInt(_min,_max,_CurVal);
+                }
+        }
+        CircularInt CircularInt::operator * (int a) {
+                int x=this->min();
+                int b=this->max();
+                int c=this->CurVal()*a;
+                return  CircularInt(x,b,c);
+                
+        }
+        CircularInt CircularInt::operator % (int a) {
+                int x=this->min();
+                int b=this->max();
+                int c=this->CurVal()%a;
+                return  CircularInt(x,b,c);
+        }
+        CircularInt CircularInt::operator ^ (int a) {
+                int x=this->min();
+                int b=this->max();
+                int c=this->CurVal()^a;
+                return  CircularInt(x,b,c);
+                
+        }
+        CircularInt& CircularInt::operator++(){
+                _CurVal++;
+                normalize();
+                return *this;
+        }
+        CircularInt& CircularInt::operator -- () {
+                 _CurVal--;
+                normalize();
+                return *this;
+        }
+        CircularInt CircularInt::operator-()  {
+                CircularInt ans = CircularInt(_min,_max,(_max-_CurVal));
+                return ans;
+        }
+        CircularInt CircularInt::operator ++ (int) {
+                CircularInt ans = CircularInt(_min,_max,_CurVal);
+                _CurVal++;
+                normalize();
+                return ans;
+        }
+        CircularInt CircularInt::operator -- (int) {
+                CircularInt ans = CircularInt(_min,_max,_CurVal);
+                _CurVal--;
+                normalize();
+                return ans;
+        }
+        CircularInt& CircularInt::operator += (int a) {
+                _CurVal+=a;
+                normalize();
+                return *this;
+        }
+        CircularInt& CircularInt::operator -= (int a) {
+                _CurVal-=a;
+                normalize();
+                return *this;
+        }
+        CircularInt& CircularInt::operator *= (int a) {
+                _CurVal*=a;
+                normalize();
+                return *this;}
+        CircularInt& CircularInt::operator /= (int a) {
+                if(modDivide(_CurVal,a,_max)!=-1){
+                _CurVal=modDivide(_CurVal,a,_max);
+                normalize();
+                }else{
+                throw new std::string("Division not defined");        
+                }
+                return *this; 
+        }
 
 
 
--- CircularInt.hpp
+++ CircularInt.hpp
@@ -15,15 +15,23 @@ class CircularInt {
         CircularInt (int a ,int b){  
             _min=a;
             _max=b;
-            this->_CurVal = _min;
+            _CurVal = _min;
         }
         
         CircularInt (int a ,int b,int c){  
             _min=a;
             _max=b;
-            this->_CurVal = ((c-1)%b)+1;
+            _CurVal = c;
+            normalize();
         }
-        
+        void normalize(){
+            while(_CurVal<_min){
+                _CurVal+=_max;
+            }
+            while(_CurVal>_max){
+                _CurVal-=_max;
+            }
+        }
         //getters:
 	    int CurVal() const {
 		return _CurVal;
@@ -48,28 +56,33 @@ class CircularInt {
         CircularInt operator * (int a) ;
         CircularInt operator % (int a) ;
         CircularInt operator ^ (int a) ;
-        CircularInt operator ++();
-        CircularInt operator --() ;
+        CircularInt& operator ++();
+        CircularInt& operator --() ;
         CircularInt operator -() ;
         CircularInt operator ++ (int) ;
         CircularInt operator -- (int) ;
-        CircularInt operator += (int a) ;
-        CircularInt operator -= (int a) ;
-        CircularInt operator *= (int a) ;
+        CircularInt& operator += (int a) ;
+        CircularInt& operator -= (int a) ;
+        CircularInt& operator *= (int a) ;
+        CircularInt& operator /= (int a) ;
         ostream& operator<< (ostream& os) ;
-
+        friend istream& operator>> (istream& is, CircularInt& c) ; 
 
 };
 inline ostream& operator<< (ostream& os, const CircularInt& c) {
-
     os << c.CurVal() ;
     return os;
 }
+inline istream& operator>> (istream& is, CircularInt& c)  
+{  
+    is>> c._min>> c._max>> c._CurVal;  
+    return is;  
+}  
 inline const CircularInt operator+(int a, const CircularInt& c2) {
-    return CircularInt(c2.min(),c2.max(),c2.CurVal()+a);
+    return CircularInt(c2.min(),c2.max(),a+c2.CurVal());
 }
 inline const CircularInt operator-(int a, const CircularInt& c2) {
-    return CircularInt(c2.min(),c2.max(),c2.CurVal()-a);
+    return CircularInt(c2.min(),c2.max(),a+(c2.max()-c2.CurVal()));
 }
 //<	>	<=	>=	++	--
 //<<	>>	==	!=
